ccee7b5f7eb3425683413880212633137fa2a645
Werton Guimaraes <werton007@gmail.com>
2019-11-06 14:33:12 -0300

    t0001 - add docker config

---
+++ util/constants.py
@@ -0,0 +1,12 @@
+unidades = ["", "um", "dois","tres", "quatro","cinco", "seis", "sete", "oito", "nove"]
+dezenas = ["", "dez", "vinte", "trinta", "quarenta", "cinquenta", "sessenta", "setenta", "oitenta", "noventa"]
+dezenas_especiais = ["", "onze", "doze", "treze", "cartoze", "quinze", "dezeseis", "dezesete", "dezoito", "dezenove"]
+centenas = ["", "cento", "duzentos", "trezentos", "quatrocentos", "quinhentos", "seicentos", "oitocentos", "novecentos"]
+
+extensoes_singular = ["", " mil", " milhao"]
+extensoes_plural = ["", " mil", " milhoes"]
+
+
+ZERO = "zero"
+CEM = "cem"
+CONCATENADOR_E = " e "
